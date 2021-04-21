522e60617dec2b688c962c72cd8c9b59884cd68a
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-04-21 21:15:31 +0100

    tried using the collection name instead of database name

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/fitnessTracker', {
+    process.env.MONGODB_URI || 'mongodb://localhost/exercises', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
